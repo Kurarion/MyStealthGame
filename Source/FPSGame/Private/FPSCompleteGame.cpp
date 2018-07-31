5e2a8ff570734c16fca59d19fe16b5ed2375f01c
sirokuma <sirokuma@irisu.cc>
2018-07-31 17:28:11 +0800

    cpp for complete game

---
+++ Source/FPSGame/Private/FPSCompleteGame.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSCompleteGame.h"
+#include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
+
+// Sets default values
+AFPSCompleteGame::AFPSCompleteGame()
+{
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	BoxComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	BoxComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	BoxComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	BoxComp->SetBoxExtent(FVector(200.0f));
+	BoxComp->SetHiddenInGame(false);
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSCompleteGame::HandleCollision);
+	RootComponent = BoxComp;
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->DecalSize = FVector(200.0f, 200.0f, 200.0f);
+	DecalComp->SetupAttachment(BoxComp);
+
+}
+
+void AFPSCompleteGame::HandleCollision(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	UE_LOG(LogTemp, Log, TEXT("OK!!!"));
+}
---
+++ Source/FPSGame/Public/FPSCompleteGame.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FPSCompleteGame.generated.h"
+
+class UBoxComponent;
+class UDecalComponent;
+
+UCLASS()
+class FPSGAME_API AFPSCompleteGame : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFPSCompleteGame();
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UBoxComponent* BoxComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+		UDecalComponent* DecalComp;
+	UFUNCTION()
+		void HandleCollision(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	
+	
+};
